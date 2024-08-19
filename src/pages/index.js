b5f32aae9429d32b00d448bf3fae2a3eea0cdc8f
Ayush <ayushtomar8624@gmail.com>
2024-08-19 15:57:39 +0530

    intial commit basic user interface

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Home from './Home.jsx';
+import Login from './Login.jsx';
+import Post from './Post.jsx';
+import Signup from './Signup.jsx';
+import EditPost from './EditPost.jsx';
+import AllPosts from './AllPosts.jsx';
+import AddPost from './AddPost.jsx';
+
+export { Home, Login, Post, Signup, EditPost, AllPosts, AddPost };
